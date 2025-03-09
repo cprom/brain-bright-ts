191d4c04a165e749fc2cbde31704fc18dea52b25
Chen Prom <cprom@Chens-MBP.lan>
2025-03-08 17:29:13 -0800

    add Temp home page card

--- src/Pages/home/index.tsx
+++ src/Pages/home/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { useAuth } from '../../contexts/authContext'
-import { Box, Button } from '@mui/material'
 import { Navigate } from 'react-router-dom'
+
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Home = () => {
     const { userLoggedIn } = useAuth();
@@ -12,10 +18,19 @@ const Home = () => {
             userLoggedIn
             ?
             <>
-
-            <Box sx={{p:5}}>
-            <h1>Home</h1>
-            </Box>
+            <Card sx={{ mt: 5, p: 10,minWidth: 275, minHeight: 300 }}>
+                <CardContent>
+                    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 50 }}>
+                    Welcome to Brain Bright.
+                    </Typography>
+                    <Typography sx={{ color: '#B197FC', fontSize: 50 }}>
+                        Learn
+                    </Typography>
+                    <Typography sx={{ color: '#63E6bE', fontSize: 50 }}>
+                        Grow
+                    </Typography>
+                </CardContent>
+                </Card>
             </>
             :
             <Navigate to="/login"/>  
