7293b382f4fdc9205da2443115cfb69c59e0224a
Chen Prom <cprom@Chens-MBP.lan>
2025-03-16 15:49:40 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/brain-bright-ts",
+  base: "/brain-bright-ts/",
   plugins: [react()],
 })
