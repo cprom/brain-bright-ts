57373bd3584fa1aa3f9681cf6fced4bbc1320a3b
Chen Prom <cprom@Chens-MBP.lan>
2025-06-20 17:27:53 -0700

    update styling to MUI, add icons to buttons

--- src/components/hand-writing/hand-writing.tsx
+++ src/components/hand-writing/hand-writing.tsx
@@ -3,11 +3,16 @@ import {
   type ReactSketchCanvasRef,
 } from "react-sketch-canvas";
 import { type ChangeEvent, useRef, useState } from "react";
+import { Button, Stack, Typography } from "@mui/material";
+
+import ModeIcon from '@mui/icons-material/Mode';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEraser, faInfo, faPen, faRedo, faTrash, faUndo } from "@fortawesome/free-solid-svg-icons";
 
  function Canvas() {
   const canvasRef = useRef<ReactSketchCanvasRef>(null);
   const [eraseMode, setEraseMode] = useState(false);
-  const [strokeWidth, setStrokeWidth] = useState(5);
+  const [strokeWidth, setStrokeWidth] = useState(2);
   const [eraserWidth, setEraserWidth] = useState(10);
 
   const handleStrokeWidthChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -47,24 +52,24 @@ import { type ChangeEvent, useRef, useState } from "react";
 
   return (
     <div className="d-flex flex-column gap-2 p-2">
-      <h1>Tools</h1>
-      <label htmlFor="strokeWidth" className="form-label">
-          Stroke width
-        </label>
+      <Typography>Tools</Typography>
+      <Typography >
+          Pen width
+        </Typography>
         <input
           disabled={eraseMode}
           type="range"
           className="form-range"
           min="1"
-          max="20"
+          max="10"
           step="1"
           id="strokeWidth"
           value={strokeWidth}
           onChange={handleStrokeWidthChange}
         />
-        <label htmlFor="eraserWidth" className="form-label">
+        <Typography>
           Eraser width
-        </label>
+        </Typography>
         <input
           disabled={!eraseMode}
           type="range"
@@ -76,54 +81,59 @@ import { type ChangeEvent, useRef, useState } from "react";
           value={eraserWidth}
           onChange={handleEraserWidthChange}
         />
-      <div className="d-flex gap-2 align-items-center ">
-        <button
-          type="button"
-          className="btn btn-sm btn-outline-primary"
+      <Stack spacing={1}>
+        <Button
           disabled={!eraseMode}
           onClick={handlePenClick}
+          variant="outlined"
+          size="small"
+          startIcon={<FontAwesomeIcon icon={faPen} />}
         >
           Pen
-        </button>
-        <button
-          type="button"
-          className="btn btn-sm btn-outline-primary"
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
           disabled={eraseMode}
           onClick={handleEraserClick}
+          startIcon={<FontAwesomeIcon icon={faEraser} />}
         >
           Eraser
-        </button>
+        </Button>
         <div className="vr" />
-        <button
-          type="button"
-          className="btn btn-sm btn-outline-primary"
+        <Button
+          variant="outlined"
+          size="small"
           onClick={handleUndoClick}
+          startIcon={<FontAwesomeIcon icon={faUndo} />}
         >
           Undo
-        </button>
-        <button
-          type="button"
-          className="btn btn-sm btn-outline-primary"
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
           onClick={handleRedoClick}
+          startIcon={<FontAwesomeIcon icon={faRedo} />}
         >
           Redo
-        </button>
-        <button
-          type="button"
-          className="btn btn-sm btn-outline-primary"
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
           onClick={handleClearClick}
+          startIcon={<FontAwesomeIcon icon={faTrash} />}
         >
           Clear
-        </button>
-        <button
-          type="button"
-          className="btn btn-sm btn-outline-primary"
+        </Button>
+        {/* <Button
+          variant="outlined"
+          size="small"
           onClick={handleResetClick}
         >
           Reset
-        </button>
-      </div>
-      <h1>Canvas</h1>
+        </Button> */}
+      </Stack>
+      <Typography>Scratch Pad</Typography>
       <ReactSketchCanvas 
         ref={canvasRef}
         strokeWidth={strokeWidth}
