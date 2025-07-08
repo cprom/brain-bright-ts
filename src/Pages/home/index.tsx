8a42d7024264d9332b916cb0870fd5024b39c22a
Chen Prom <cprom@Chens-MBP.lan>
2025-07-07 20:04:59 -0700

    update homepage

--- src/Pages/home/index.tsx
+++ src/Pages/home/index.tsx
@@ -6,23 +6,20 @@ const Home = () => {
 
 
     return (
-        
         <>
-                <div style={{marginTop: 100, textAlign: 'center'}}>
-                    <Typography className='outline-letter' gutterBottom sx={{ color: '#B197FC', fontSize: 50, fontWeight: 'bolder'}}>
-                    Welcome to <span style={{color: '#FFD43B'}}>Brain</span> <span style={{color:'#63E6bE'}}>Bright</span>.  <span style={{color:'#FFA047'}}>Learn</span>, <span style={{color:"#FF6161"}}>Grow</span>, Have <span style={{color: '#74C0FC'}}>Fun</span>.
-                    </Typography>
-                </div>
+            <div style={{marginTop: 100, textAlign: 'center', padding: 5}}>
+                <Typography className='outline-letter' gutterBottom sx={{ color: '#B197FC', fontSize: 50, fontWeight: 'bolder'}}>
+                Welcome to <span style={{color: '#FFD43B'}}>Brain</span> <span style={{color:'#63E6bE'}}>Bright</span>.  <span style={{color:'#FFA047'}}>Learn</span>, <span style={{color:"#FF6161"}}>Grow</span>, Have <span style={{color: '#74C0FC'}}>Fun</span>.
+                </Typography>
+            </div>
             <Box sx={{ minHeight: 300, maxWidth: '1000px', textAlign: 'center', margin: 'auto' }}   style={{
                 backgroundImage: `url(${kidsBackgroundImage3D})`,
-                backgroundSize: "cover",
+                backgroundSize: "contained",
                 height: "80vh",
                 color: "#f5f5f5",
                 backgroundPosition: 'center'}}>
-
             </Box>
         </>
-        
     )
 }
 
